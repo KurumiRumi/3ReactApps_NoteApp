3b570bf4f60121787f590c4b0702ebd01cec4c70
KurumiRumi <flora.fluorite.caf2@gmail.com>
2024-02-08 15:57:24 +0900

    52. Sidebarコンポーネントから作成しよう

--- noteapp-with-react/vite.config.js
+++ noteapp-with-react/vite.config.js
@@ -4,5 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000, host: true },
+  server: {
+    port: 3000,
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+  },
 });
